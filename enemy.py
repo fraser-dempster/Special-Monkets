1e687302215974ea482ce0a1c3f33f4c56a751b1
Paul Bekaert <44134414+paulbeka@users.noreply.github.com>
2020-10-02 20:06:53 +0100

    Create enemy.py

---
+++ enemy.py
@@ -0,0 +1,22 @@
+import pygame
+
+class Enemy:
+
+	def __init__(self, x, y):
+		self.x, self.y = x, y
+
+		self.initVars()
+
+	def initVars(self):
+		self.damage = 0
+		self.speed = 0
+		self.image = None
+
+	def attack(self):
+		pass
+
+	def update(self):
+		pass
+
+	def render(self):
+		pass
