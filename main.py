af63fdc64db2ab90e8a81664427bc6f4d3c6617a
Paul Bekaert <44134414+paulbeka@users.noreply.github.com>
2020-10-02 19:29:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,34 +3,48 @@ import threading
 
 pygame.init()
 
+WHITE = 255,255,255
+BLACK = 0,0,0
+
 class Game:
 
 	def __init__(self, width, height):
 		self.height = height
 		self.width = width
 
-		self.clock = pygame.time.Clock(60)
+		self.clock = pygame.time.Clock()
 		self.running = True
 		
 		self.initScreen()
+		self.run()
 
 	def initScreen(self):
 		self.gameScreen = pygame.display.set_mode([self.width, self.height])#
-		pygame.gameScreen.set_caption("Monkey Mullets")
+		pygame.display.set_caption("Monkey Mullets")
 
 	def run(self):
 		
 		while self.running:
+			self.gameScreen.fill(WHITE) #Reset game screen
 
+			self.update()
+			self.render()
+
+			self.clock.tick(60)
 
 	def update(self):
-		pass
+		
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				self.running = False
+				pygame.quit()
 
 	def render(self):
-		pass
+
+		pygame.display.update()
 
 def main():
-	pass
+	game = Game(800,800)
 
 if __name__ == "__main__":
 	main()
