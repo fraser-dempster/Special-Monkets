76b5d7ebc5cc07253df247ea7a7313d0dadfbd38
Paul Bekaert <44134414+paulbeka@users.noreply.github.com>
2020-10-03 14:29:02 +0100

    Added all pictures

--- Player.py
+++ Player.py
@@ -2,11 +2,12 @@ import pygame
 import math
 
 class Player(object):
-    def __init__(self, x, y, radius):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.radius = radius
         self.rect = pygame.Rect(x, y, 10, 10)
+        self.image = pygame.image.load("images\\MulletMonkey_Idle\\sprite_0.png")
 
     def render(self, display):
-        pygame.draw.rect(display, (255, 0, 0), self.rect, 50)
+        #pygame.draw.rect(display, (255, 0, 0), self.rect, 50)
+        display.blit(self.image, self.rect)
